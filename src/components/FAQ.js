4d1af389381874bee5c7fb88d44e764a6965bbd1
Amirali Rezaei <exjnnd@gmail.com>
2022-09-28 07:42:00 +0330

    Added FAQ Section

---
+++ src/components/FAQ.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FAQ = () => {
+	return (
+		<div className="faq">
+			<h2>
+				Any questions <span>FAQ</span>.
+			</h2>
+			<div className="question">
+				<h4>How do I start?</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor sit, amet consectetur adipisicing
+						elit. Quidem, magni.
+					</p>
+				</div>
+			</div>
+			<div className="question">
+				<h4>Daily schedule</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor sit, amet consectetur adipisicing
+						elit. Quidem, magni.
+					</p>
+				</div>
+			</div>
+			<div className="question">
+				<h4>Diffrent payment methods</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor sit, amet consectetur adipisicing
+						elit. Quidem, magni.
+					</p>
+				</div>
+			</div>
+			<div className="question">
+				<h4>What products do you offer?</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor sit, amet consectetur adipisicing
+						elit. Quidem, magni.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default FAQ;
